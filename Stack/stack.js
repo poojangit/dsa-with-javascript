2f199a7a102ec623525bff4b0880358fab5f949b
poojangit <poojang147@gmail.com>
2024-12-09 12:48:22 +0530

    [POOJA] theory : theory part on stack

---
+++ Stack/stack.js
@@ -0,0 +1,34 @@
+//todo ---------STACK--------------------
+
+//^ Stack 
+//! Stack is linear data structure that follows LIFO
+//! Last element added will be the first element to get removed
+
+//^ Stack operations 
+//! push - pushes the element to the top of the stack
+//! pop - removes the element from the top of the stack
+//! peek - get the value of top element without removing it
+//! isEmpty - check if the stack is empty
+//! size - return the number/size of the elements
+//! clear - removes the elements from the stack
+
+//^ Advantages of stacks
+//! Simplicity
+//! Efficiency
+//! memory management
+
+//^ Disadvantages of stacks
+//! Only the top element will be accessed directly
+//! Overflow and underflow
+
+//^ stack use in program
+//! Managing the function call in recursion
+//! undo/redo functionality in text editors
+//! Browser history navigation
+//! Reversing data
+//! function call stack
+//! Managing temporary data
+
+//^ stack's real time use
+//! undo operatons
+//! stack of plates
