40b57fd39c1b13f447c0b1e4cb1213a7a8fea417
poojangit <poojang147@gmail.com>
2024-12-11 10:36:38 +0530

    [POOJA] QUEUE : Task scheduling using queue

--- Queue/queue.js
+++ Queue/queue.js
@@ -19,54 +19,141 @@
 //* Queue Implemenation using arrays
 //* Queue Implemenation using linked list
 
-class Queue{
-    constructor() {
-        this.queueItems = []
-    }
-    enqueue(element){
-        this.queueItems.push(element)
-    }
-    dequeue() {
-        if(this.isEmpty()){
-            return "Queue is empty"
-        }
-        return this.queueItems.pop()
-    }
-    front() {
-        if(this.isEmpty()){
-            return "Queue is empty"
-        }
-        return this.queueItems[0]
-    }
-    size() {
-       return this.queueItems.length
-    }
-    isEmpty() {
-        return this.size() == 0
-    }
-    clear() {
-        return this.queueItems  = []
+//* Queue Implemenation using arrays
+// class Queue{
+//     constructor() {
+//         this.queueItems = []
+//     }
+//     enqueue(element){
+//         this.queueItems.push(element)
+//     }
+//     dequeue() {
+//         if(this.isEmpty()){
+//             return "Queue is empty"
+//         }
+//         return this.queueItems.pop()
+//     }
+//     front() {
+//         if(this.isEmpty()){
+//             return "Queue is empty"
+//         }
+//         return this.queueItems[0]
+//     }
+//     size() {
+//        return this.queueItems.length
+//     }
+//     isEmpty() {
+//         return this.size() == 0
+//     }
+//     clear() {
+//         return this.queueItems  = []
+//     }
+// }
+// const queue = new Queue()
+// console.log(queue);
+// console.log(queue.isEmpty());
+// queue.enqueue(10)
+// console.log(queue);
+// console.log(queue.size());
+// console.log(queue.isEmpty());
+// console.log(queue.dequeue());
+// console.log(queue.isEmpty());
+// console.log(queue.front());
+// queue.enqueue(20)
+// queue.enqueue(30)
+// queue.enqueue(40)
+// console.log(queue.front());
+// console.log(queue);
+// console.log(queue.clear());
+// console.log(queue);
+
+//* Queue Implemenation using linked list
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
     }
 }
-const queue = new Queue()
+class LinkedListQueue {
+    constructor() {
+        this.front = null
+        this.rear = null
+        this.size = 0
+    }
+    enqueue(value){
+        const newNode = new Node(value)
+        if(this.rear) {
+            this.rear.next = newNode
+        }
+        this.rear = newNode
+        if(!this.front){
+            this.front = newNode
+        }
+        this.size ++
+    }
+    dequeue(){
+        if(this.isEmpty()){
+            return "queue is empty"
+        }
+        const dequeuedValue = this.front.value
+        this.front = this.front.next
+        if(!this.front){
+            this.rear = null
+        }
+        this.size --
+        return dequeuedValue
+    }
+    peek() {
+        if(this.isEmpty()){
+            return 'queue is empty'
+        }
+        return this.front.value
+    }
+    isEmpty() {
+        return this.size == 0
+    }
+    print() {
+        let current = this.front
+        let result = []
+        while(current) {
+            result.push(current.value)
+            current = current.next
+        }
+        console.log(result.join(", "));
+        
+    }
+ 
+}
+const queue = new LinkedListQueue()
 console.log(queue);
 console.log(queue.isEmpty());
 queue.enqueue(10)
 console.log(queue);
-console.log(queue.size());
+console.log(queue.size);
 console.log(queue.isEmpty());
+queue.enqueue(100)
+queue.enqueue(200)
+queue.enqueue(300)
+console.log(queue);
 console.log(queue.dequeue());
-console.log(queue.isEmpty());
-console.log(queue.front());
-queue.enqueue(20)
-queue.enqueue(30)
-queue.enqueue(40)
-console.log(queue.front());
 console.log(queue);
-console.log(queue.clear());
+console.log(queue.dequeue());
+console.log(queue.peek());
 console.log(queue);
+// console.log(queue.dequeue());
+// console.log(queue.dequeue());
+queue.enqueue(400)
+queue.print()
 
+//~ Example 1 - Task scheduling
 
-
+const taskQueue = new LinkedListQueue()
+taskQueue.enqueue("Task 1")
+taskQueue.enqueue("Task 2")
+taskQueue.enqueue("Task 3")
+while(!taskQueue.isEmpty()){
+    console.log("Processing " , taskQueue.dequeue());
+}
 
 
