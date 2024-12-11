ef777e3dd9c62beca0cf928228758fdf163b8adf
poojangit <poojang147@gmail.com>
2024-12-11 14:07:08 +0530

    [POOJA] LinkedList : Overview of LinkedList, Types of Linked List and its advantage and disadvantage

---
+++ LinkedList/linkedList.js
@@ -0,0 +1,52 @@
+//todo ----------------LINKED LIST--------------------------------
+
+//^ Linked list is a data structure where elements are connected in a linear sequence using pointers.
+//^ Linked list contains two main components
+//! Data and the Node
+//^ unlike arrays linked list do not store elements in the contiguous memory locations , which makes them dynamic and efficient for certain operations like insertion and deletion
+
+//^  Types of Linked List
+//* Singly Linked List
+//* doubly Linked List
+//* Circular linked list
+//* doubly circular linked list
+
+//* Singly linked list
+//! Each node points to the next node in the sequence
+//! last node's pointer is null
+//! Operations like traversal are unidirectional
+//* doubly linked list
+//! each node contains two pointers -
+//! one points to the next node and another points to the previous node
+//! allows bidirectional traveral
+//* circlular linked list
+//! the last node points back to the head , forming a circle
+//! Can be singly or doubly linked
+//! No null pointers in the list
+//* doubly linked list
+//! similar to circular linked list but with bidirectional
+
+//^ Advantages of Linked List
+//! Dynamic size
+//! Efficient Insertion/Deletion
+//! Memory utilization
+//! Ease of implemenation
+
+//^ Disadvantages of Linked list
+//! Memory Overhead
+//! Sequential access
+//! Complexity
+//! Cache performance
+
+//^ Basic Opeartions on Linked List
+//! Traversal - visiting each node in list, O(n)
+//! Insertion - adding the new node in the list
+//! Search
+//! Reversal
+
+//^ Application of Linked List
+//! Dynamic Memory allocation
+//! Implementation of other data structure - stacks and queues 
+//! Undo functionality
+//! Navigation System
+
